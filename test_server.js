97744e95a3267ba64e3f91c9bef591aba78cf67d
Pomidor04ka <100572668+Pomidor04ka@users.noreply.github.com>
2025-07-28 23:49:22 +0300

    Delete test_server.js

--- test_server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/test", (req, res) => {
-    res.send("Тестовый сервер работает!");
-});
-
-app.listen(3000, () => console.log("Тестовый сервер запущен на порту 3000"));
